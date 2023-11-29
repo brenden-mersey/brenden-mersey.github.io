1057a9ad315d98020ba8052466167d765b29a005
havemersey <brenden@daymarker.digital>
2023-11-28 20:16:13 -0800

    QC

--- projects/ravenous/src/js/components/SearchBar/SearchBar.js
+++ projects/ravenous/src/js/components/SearchBar/SearchBar.js
@@ -24,47 +24,50 @@ function SearchBar(props) {
     }
   };
   
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  
   function getRandomHeroImagePath() {
     const randomNumber = Math.floor(Math.random() * (3 - 1 + 1) + 1);
     return `/img/search-hero-0${randomNumber}.jpg`;
   }
 
   return (
-    <>
-      <div className="search-bar">
-        <div className="search-bar__image">
-          <img src={getRandomHeroImagePath()} alt="Search Bar Hero" />
-        </div>
-        <div className="search-bar__container grid-container">
-          <form className="search-bar__form form">
-            <div className="search-bar__form-row">
-              <div className="search-bar__form-field">
-                <input 
-                  className="search-bar__input input" 
-                  name="search"
-                  id="search" 
-                  type="text" 
-                  placeholder="What are you looking for?"
-                  value={searchValue}
-                  onChange={handleInputChange} />
-              </div>
-              <div className="search-bar__form-field">
-                <input 
-                  className="search-bar__input input" 
-                  name="location" 
-                  id="location" 
-                  type="text" 
-                  placeholder="In what city?"
-                  value={locationValue} 
-                  onChange={handleInputChange} />
-              </div>
-            </div>
-          </form>
-        </div>
+    <div className="search-bar">
+      <div className="search-bar__image">
+        <img src={getRandomHeroImagePath()} alt="Search Bar Hero" />
       </div>
-      <div>Search: <strong>{searchValue}</strong></div>
-      <div>Location: <strong>{locationValue}</strong></div>
-    </>
+      <div className="search-bar__container grid-container">
+        <form className="search-bar__form form" onSubmit={handleSubmit}>
+          <div className="search-bar__form-row">
+            <div className="search-bar__form-field">
+              <input 
+                className="search-bar__input input" 
+                name="search"
+                id="search" 
+                type="text" 
+                placeholder="What are you looking for?"
+                value={searchValue}
+                onChange={handleInputChange} />
+            </div>
+            <div className="search-bar__form-field">
+              <input 
+                className="search-bar__input input" 
+                name="location" 
+                id="location" 
+                type="text" 
+                placeholder="In what city?"
+                value={locationValue} 
+                onChange={handleInputChange} />
+            </div>
+          </div>
+          <div className="search-bar__form-action">
+            <button className="search-bar__button button" type="submit">Let's Go Baby.</button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 
 }
